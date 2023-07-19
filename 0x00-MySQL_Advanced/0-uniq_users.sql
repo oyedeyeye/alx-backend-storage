ed4a25ac10b8af95d4068a3ec69445050ea76481
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-19 09:56:27 +0100

    task 0: Unique attributes

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- script that creates a table users following these requirements:
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    UNIQUE (email),
+    PRIMARY KEY (id)
+);
