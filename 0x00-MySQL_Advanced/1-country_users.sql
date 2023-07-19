6317dc4309e6ba10d9fbc41e9d71e95a20605410
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-19 11:00:28 +0100

    task 1: ENUM attributes

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- script that creates a table users following these requirements:
-CREATE TABLE IIF NOT EXITS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXITS users (
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US','CO','TN') DEFAULT 'US'
+    country ENUM('US','CO','TN') DEFAULT 'US',
+    PRIMARY KEY (id)
 );
